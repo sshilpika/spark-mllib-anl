fd8baf0f860612bffcda610cce841169943ea3c8
Shilpika S <sshilpika5@gmail.com>
2016-07-08 17:10:44 -0500

    First commit

---
+++ build.sbt
@@ -0,0 +1,28 @@
+name := "spark-mllib-anl"
+
+version := "1.0"
+
+scalaVersion := "2.10.6"
+
+scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked")
+
+mainClass in assembly := Some("SparkRandomForestClassification")
+
+resolvers ++= Seq(
+  "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+)
+
+resolvers += Resolver.sonatypeRepo("public")
+
+libraryDependencies ++= Seq(
+
+  "org.apache.spark" % "spark-sql_2.10" % "1.6.1" % "provided",
+  "org.apache.spark" % "spark-core_2.10" % "1.6.1" % "provided",
+  "com.github.scopt" %% "scopt" % "3.5.0",
+  "org.scalatest" %% "scalatest" % "2.2.6" % Test,
+  "com.squants" % "squants_2.10" % "0.6.2",
+  "com.databricks" % "spark-csv_2.10" % "1.4.0",
+  "org.apache.spark" % "spark-mllib_2.11" % "1.6.1"
+)
+
+
---
+++ src/main/scala/SparkRandomForestClassification.scala
@@ -0,0 +1,49 @@
+
+package org.apache.spark.mllib
+
+import org.apache.spark.{SparkConf, SparkContext}
+// $example on$
+import org.apache.spark.mllib.tree.RandomForest
+import org.apache.spark.mllib.tree.model.RandomForestModel
+import org.apache.spark.mllib.util.MLUtils
+// $example off$
+
+object SparkRandomForestClassification {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("RandomForestClassification")
+    val sc = new SparkContext(conf)
+    // $example on$
+    // Load and parse the data file.
+    val data = MLUtils.loadLibSVMFile(sc, "/home/shilpika/anl/mllibSpark/code/batch_1_libsvm.txt")
+    // Split the data into training and test sets (30% held out for testing)
+    val splits = data.randomSplit(Array(0.7, 0.3))
+    val (trainingData, testData) = (splits(0), splits(1))
+
+    // Train a RandomForest model.
+    // Empty categoricalFeaturesInfo indicates all features are continuous.
+    val numClasses = 10
+    val categoricalFeaturesInfo = Map[Int, Int]()
+    val numTrees = 1000 // Use more in practice.
+    val featureSubsetStrategy = "auto" // Let the algorithm choose.
+    val impurity = "gini"
+    val maxDepth = 20
+    val maxBins = 1000
+
+    val model = RandomForest.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+      numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
+
+    // Evaluate model on test instances and compute test error
+    val labelAndPreds = testData.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+    val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.count()
+    println("Test Error = " + testErr)
+    println("Learned classification forest model:\n" + model.toDebugString)
+
+    // Save model
+    model.save(sc, "myRandomForestClassificationModel")
+   // val sameModel = RandomForestModel.load(sc, "myRandomForestClassificationModel")
+    // $example off$
+  }
+}
