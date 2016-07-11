59344f5ac6511b400b87c8ad22a75c1c794b6f4b
Shilpika S <sshilpika5@gmail.com>
2016-07-11 13:39:21 -0500

    testing jlse manual cluster

---
+++ src/main/scala/KmeansTest.scala
@@ -0,0 +1,33 @@
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.sql.DataFrame
+
+
+class KmeansTest {
+  // Crates a DataFrame
+
+  val conf = new SparkConf().setAppName("KMEANS-TEST")
+  val sc: SparkContext = new SparkContext(conf)
+  val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+  val dataset: DataFrame = sqlContext.createDataFrame(Seq(
+    (1, Vectors.dense(0.0, 0.0, 0.0)),
+    (2, Vectors.dense(0.1, 0.1, 0.1)),
+    (3, Vectors.dense(0.2, 0.2, 0.2)),
+    (4, Vectors.dense(9.0, 9.0, 9.0)),
+    (5, Vectors.dense(9.1, 9.1, 9.1)),
+    (6, Vectors.dense(9.2, 9.2, 9.2))
+  )).toDF("id", "features")
+
+  // Trains a k-means model
+  val kmeans = new KMeans()
+    .setK(2)
+    .setFeaturesCol("features")
+    .setPredictionCol("prediction")
+  val model = kmeans.fit(dataset)
+
+  // Shows the result
+  println("Final Centers: ")
+  model.clusterCenters.foreach(println)
+}
