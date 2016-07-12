525b31f119945909c816501533589941d299d8f4
Shilpika S <sshilpika5@gmail.com>
2016-07-12 13:19:32 -0500

    nodes in output layer change

--- src/main/scala/NN.scala
+++ src/main/scala/NN.scala
@@ -30,7 +30,7 @@ object NN {
     val test = splits(1)
     // specify layers for the neural network:
     // input layer of size 4 (features), two intermediate of size 5 and 4 and output of size 3 (classes)
-    val layers = Array[Int](3072, 5, 4, 3072)
+    val layers = Array[Int](3072, 5, 4, 255)
     // create the trainer and set its parameters
     val trainer = new MultilayerPerceptronClassifier()
       .setLayers(layers)
